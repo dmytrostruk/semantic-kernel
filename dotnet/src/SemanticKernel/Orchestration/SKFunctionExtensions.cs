9bd91f047510a6ddf3ad1263407715b676294474
Dmytro Struk <dmytrostruk@microsoft.com>
2023-03-26 20:32:16 +0100

    Reverted removed methods

--- dotnet/src/SemanticKernel/Orchestration/SKFunctionExtensions.cs
+++ dotnet/src/SemanticKernel/Orchestration/SKFunctionExtensions.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
 using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
@@ -16,6 +17,77 @@ namespace Microsoft.SemanticKernel.Orchestration;
 /// </summary>
 public static class SKFunctionExtensions
 {
+    /// <summary>
+    /// Configure the LLM settings used by semantic function.
+    /// </summary>
+    /// <param name="skFunction">Semantic function</param>
+    /// <param name="settings">Backend settings</param>
+    /// <returns>Self instance</returns>
+    public static ISKFunction UseCompletionSettings(this ISKFunction skFunction, IDictionary<string, object> settings)
+    {
+        return skFunction.SetAIConfiguration(settings);
+    }
+
+    /// <summary>
+    /// Change the LLM Max Tokens configuration
+    /// </summary>
+    /// <param name="skFunction">Semantic function</param>
+    /// <param name="maxTokens">Tokens count</param>
+    /// <returns>Self instance</returns>
+    public static ISKFunction UseMaxTokens(this ISKFunction skFunction, int maxTokens)
+    {
+        skFunction.BackendSettings.TryAdd("MaxTokens", maxTokens);
+        return skFunction;
+    }
+
+    /// <summary>
+    /// Change the LLM Temperature configuration
+    /// </summary>
+    /// <param name="skFunction">Semantic function</param>
+    /// <param name="temperature">Temperature value</param>
+    /// <returns>Self instance</returns>
+    public static ISKFunction UseTemperature(this ISKFunction skFunction, double temperature)
+    {
+        skFunction.BackendSettings.TryAdd("Temperature", temperature);
+        return skFunction;
+    }
+
+    /// <summary>
+    /// Change the Max Tokens configuration
+    /// </summary>
+    /// <param name="skFunction">Semantic function</param>
+    /// <param name="topP">TopP value</param>
+    /// <returns>Self instance</returns>
+    public static ISKFunction UseTopP(this ISKFunction skFunction, double topP)
+    {
+        skFunction.BackendSettings.TryAdd("TopP", topP);
+        return skFunction;
+    }
+
+    /// <summary>
+    /// Change the Max Tokens configuration
+    /// </summary>
+    /// <param name="skFunction">Semantic function</param>
+    /// <param name="presencePenalty">Presence penalty value</param>
+    /// <returns>Self instance</returns>
+    public static ISKFunction UsePresencePenalty(this ISKFunction skFunction, double presencePenalty)
+    {
+        skFunction.BackendSettings.TryAdd("PresencePenalty", presencePenalty);
+        return skFunction;
+    }
+
+    /// <summary>
+    /// Change the Max Tokens configuration
+    /// </summary>
+    /// <param name="skFunction">Semantic function</param>
+    /// <param name="frequencyPenalty">Frequency penalty value</param>
+    /// <returns>Self instance</returns>
+    public static ISKFunction UseFrequencyPenalty(this ISKFunction skFunction, double frequencyPenalty)
+    {
+        skFunction.BackendSettings.TryAdd("FrequencyPenalty", frequencyPenalty);
+        return skFunction;
+    }
+
     /// <summary>
     /// Execute a function with a custom set of context variables.
     /// Use case: template engine: semantic function with custom input variable.
